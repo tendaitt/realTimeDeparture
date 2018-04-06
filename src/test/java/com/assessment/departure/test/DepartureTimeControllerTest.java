b1aa5384a02a339336b7206d9f16ee29e4a85932
tendaitt <tendaitt@gmail.com>
2018-04-06 09:22:57 +0200

    initial test files

---
+++ src/test/java/com/assessment/departure/test/DepartureTimeControllerTest.java
@@ -0,0 +1,14 @@
+package com.assessment.departure.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepartureTimeControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
