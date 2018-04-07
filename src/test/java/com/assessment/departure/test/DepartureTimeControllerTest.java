67f3182d961d602d74618cf60e7afb9b0cd51552
tendaitt <tendaitt@gmail.com>
2018-04-07 10:18:11 +0200

    add initial junit tests

--- src/test/java/com/assessment/departure/test/DepartureTimeControllerTest.java
+++ src/test/java/com/assessment/departure/test/DepartureTimeControllerTest.java
@@ -1,14 +1,25 @@
 package com.assessment.departure.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+
+import com.assessment.departure.DepartureInfo;
+import com.assessment.departure.DepartureTimeController;
 
 public class DepartureTimeControllerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetDepartureTimeReturnsDepartureInfo() {
+		DepartureTimeController departureControllerMock = mock(DepartureTimeController.class);
+		DepartureInfo departureInfo = mock(DepartureInfo.class);
+		
+		when(departureControllerMock.getDepartureTime(1)).thenReturn(departureInfo);
+		
+		assertEquals(departureControllerMock.getDepartureTime(1), departureInfo);
+		
 	}
 
 }
--- src/test/java/com/assessment/departure/test/NextBusDAOTest.java
+++ src/test/java/com/assessment/departure/test/NextBusDAOTest.java
@@ -1,14 +1,26 @@
 package com.assessment.departure.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+
+import com.assessment.departure.DepartureInfo;
+import com.assessment.departure.NextBusDAO;
 
 public class NextBusDAOTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRetrieveDepartureInfoReturnsDepartureInfo() {
+		NextBusDAO nextBusDAOMock = mock(NextBusDAO.class);
+		DepartureInfo departureInfo = mock(DepartureInfo.class);
+		
+		when(nextBusDAOMock.retrieveDepartureInfo()).thenReturn(departureInfo);
+		
+		assertEquals(nextBusDAOMock.retrieveDepartureInfo(), departureInfo);
+		
 	}
-
+	
+	
 }
